ebce2428856e9e23c982084583146b4519dd8d1b
maelm437 <maelmamic@gmail.com>
2025-11-18 21:07:28 +0100

    änderungen

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,30 @@
-// Einfache Variable mit Typ
-let greeting: string = "Hallo TypeScript!";
+export default function Page() {
+  // Dein TypeScript-Code kann hier oder außerhalb stehen
+  let greeting: string = "Hallo TypeScript!";
 
-// Funktion mit Typen
-function addNumbers(a: number, b: number): number {
-  return a + b;
+  function addNumbers(a: number, b: number): number {
+    return a + b;
+  }
+
+  interface User {
+    name: string;
+    age: number;
+  }
+
+  const user: User = {
+    name: "Alex",
+    age: 25
+  };
+
+  console.log(greeting);
+  console.log("Summe:", addNumbers(5, 7));
+  console.log("User:", user);
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+      <p>Summe: {addNumbers(5, 7)}</p>
+      <p>User: {user.name} ({user.age})</p>
+    </div>
+  );
 }
-
-// Ein kleines Objekt mit Typ
-interface User {
-  name: string;
-  age: number;
-}
-
-const user: User = {
-  name: "Alex",
-  age: 25
-};
-
-console.log(greeting);
-console.log("Summe:", addNumbers(5, 7));
-console.log("User:", user);
